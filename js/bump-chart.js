b6277ebf5b04ac297a32b58217f70c7706d0aea6
BrianEisnerUtah <70172073+BrianEisnerUtah@users.noreply.github.com>
2020-11-02 11:33:48 -0700

    Add code skeleton

--- js/bump-chart.js
+++ js/bump-chart.js
@@ -1,12 +1,23 @@
 /** Class implementing the bump chart. */
 class BumpChart {
 
-    constructor(data) {
+    constructor(data, updateGame, updateTeam) {
         this.data = data;
+        this.updateGame = updateGame;
+        this.updateTeam = updateTeam;
+
+        this.xAxis = "week";
+        this.yAxis = "position";
+
+        drawChart(this.xAxis, this.yAxis)
     }
 
     drawChart() {
-        
+        // Draw the chart, with appropriate axes
+    }
+
+    changeAxes(xAxis, yAxis) {
+    	// Change x-axis between week, game, and date; or y-axis between position, points, or ppg
     }
 
 }
