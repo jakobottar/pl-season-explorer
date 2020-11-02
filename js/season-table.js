efb6d459ada761ff257c88c69ead6f8bcbedbf13
BrianEisnerUtah <70172073+BrianEisnerUtah@users.noreply.github.com>
2020-11-02 10:13:12 -0700

    Create season-table.js

---
+++ js/season-table.js
@@ -0,0 +1,12 @@
+/** Class implementing the season summary table view. */
+class SeasonTable {
+
+    constructor(data) {
+        this.data = data;
+    }
+
+    drawChart() {
+        
+    }
+
+}
