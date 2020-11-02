99d3ea77aa988e62126c5dd2385c83845e33d3d6
BrianEisnerUtah <70172073+BrianEisnerUtah@users.noreply.github.com>
2020-11-02 11:37:15 -0700

    Add code skeleton

--- js/season-table.js
+++ js/season-table.js
@@ -1,12 +1,13 @@
 /** Class implementing the season summary table view. */
 class SeasonTable {
 
-    constructor(data) {
+    constructor(data, updateTeam) {
         this.data = data;
+        this.updateTeam = updateTeam;
     }
 
     drawChart() {
-        
+        // Draw the mostly-static chart
     }
 
 }
