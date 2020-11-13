cd7d6e4eeb47091fd06ef7e5eec6a57d872996c3
froggiejj <33588648+froggiejj@users.noreply.github.com>
2020-11-13 12:51:06 -0700

    added table computation

--- js/bump-chart.js
+++ js/bump-chart.js
@@ -13,7 +13,84 @@ class BumpChart {
     }
 
     drawChart() {
+        this.makeTable();
         // Draw the chart, with appropriate axes
     }
+    
+    makeTable(){
 
+        function getPoints(forScore, againstScore){
+            if(forScore > againstScore){ return 3; }
+            else if(forScore < againstScore){ return 0; }
+            else{ return 1; }
+        }
+
+        function sortTable(a, b){
+            if(a.points > b.points){ return -1; }
+            else if(a.points < b.points){ return 1; }
+            else{
+                if(a.gd > b.gd){ return -1; }
+                else if(a.gd < b.gd){ return 1; }
+                else{
+                    if(a.gs > b.gs){ return -1; }
+                    else if(a.gs < b.gs){ return 1; }
+                    else{
+                        if(a.team_name.toLowerCase() > a.team_name.toLowerCase()) { return 1; }
+                        else if(a.team_name.toLowerCase() > a.team_name.toLowerCase()) { return -1; }
+                        return 0;
+                    }
+                }
+            }
+        }
+
+        let table = [];
+        for(let i = 0; i < 38; i++){
+            let gw = this.data.filter(d => +d.gameweek == i+1);
+            let col = []
+            if(i == 0){
+                console.log(gw);
+                gw.forEach(e => {
+                    col.push({
+                        "team_name": e.home_team_name,
+                        "points": getPoints(e.home_team_goal_count, e.away_team_goal_count),
+                        "gd": e.home_team_goal_count - e.away_team_goal_count,
+                        "gs": +e.home_team_goal_count
+                    });
+                    col.push({
+                        "team_name": e.away_team_name,
+                        "points": getPoints(e.away_team_goal_count, e.home_team_goal_count),
+                        "gd": e.away_team_goal_count - e.home_team_goal_count,
+                        "gs": +e.away_team_goal_count
+                    });
+                });
+
+                col.sort(sortTable);
+                for(let p = 1; p <= col.length; p++){ col[p-1].place = p; }
+            }
+            else{
+                //NOTE: This breaks if there are <10 games per GW
+                gw.forEach(e => {
+                    //TODO: is there a better way to do this?
+                    let home = Object.assign({}, table[i-1].find(d => d.team_name == e.home_team_name));
+                    home.points += getPoints(e.home_team_goal_count, e.away_team_goal_count);
+                    home.gd += e.home_team_goal_count - e.away_team_goal_count;
+                    home.gs += +e.home_team_goal_count;
+                    col.push(home);
+
+                    let away = Object.assign({}, table[i-1].find(d => d.team_name == e.away_team_name));
+                    away.points += getPoints(e.away_team_goal_count, e.home_team_goal_count);
+                    away.gd += e.away_team_goal_count - e.home_team_goal_count;
+                    away.gs += +e.away_team_goal_count;
+                    col.push(away);
+                });
+                
+                col.sort(sortTable)
+                for(let p = 1; p <= col.length; p++){ col[p-1].place = p; }
+            }
+
+            table.push(col);
+        }
+        console.log(table)
+        return table;
+    }
 }
