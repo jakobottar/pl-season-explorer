428e055070a2a87f78929219a45b482942371702
BrianEisnerUtah <70172073+BrianEisnerUtah@users.noreply.github.com>
2020-11-17 14:27:34 -0700

    Allow deselection of game

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,23 @@
 Promise.all([d3.csv('./data/seasonData.csv'), d3.csv('./data/teamInfo.csv')]).then(data => {
 
-    // No team selected by default
+    // No team/game selected by default
     this.activeTeam = null;
+    this.activeGame = null;
 
     let that = this;
     let seasonData = data[0];
     window.teamData = data[1]; // Contains data such as team names, abbreviations, and logos. Useful across entire project, so using global variable
 
     function updateGame(gameID) {
-        gameDetail.showGame(gameID);
+        if (this.activeGame === gameID) {
+            this.activeGame = null;
+            gameDetail.clearGame();
+            gameTable.selectGame(null);
+        } else {
+            this.activeGame = gameID;
+            gameDetail.showGame(gameID);
+            gameTable.selectGame(gameID);
+        }
     }
 
     function updateTeam(teamID) {
