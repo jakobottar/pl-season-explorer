22edad61792ac459047708487e27e9b59528d73e
BrianEisnerUtah <70172073+BrianEisnerUtah@users.noreply.github.com>
2020-11-19 11:19:05 -0700

    Tidy code

--- public/js/script.js
+++ public/js/script.js
@@ -21,56 +21,27 @@ Promise.all([d3.csv('./data/seasonData.csv'), d3.csv('./data/teamInfo.csv')]).th
     }
 
     function updateTeam(teamID) {
-    	// This will update the team selected (e.g. if we want to highlight the line from a specific team on both the bump chart and the final season chart at the same time)
-        that.activeTeam = teamID;
-        if (teamID === null) {
-            finalSeasonChart.clearHighlight();
-            gameTable.clearHighlight();
-            bumpChart.clearHighlight();
-            that.activeTeam = null;
+        if (this.activeTeam === teamID) {
+            this.activeTeam = null;
+            finalSeasonChart.clearTeam();
+            gameTable.clearTeam();
+            bumpChart.clearTeam();
         } else {
-            that.activeTeam = teamID;
-            finalSeasonChart.updateHighlightClick(teamID);
-            gameTable.updateHighlightClick(teamID);
-            bumpChart.updateHighlightClick(teamID);
+            this.activeTeam = teamID;
+            finalSeasonChart.selectTeam(teamID);
+            gameTable.selectTeam(teamID);
+            bumpChart.selectTeam(teamID);
         }
-       
     }
-
-
-
-    let wrapperDiv = d3
-    .select(".wrapper");
-
-    wrapperDiv
-        .append("div")
-        .attr("id", "tooltip")
-        .style("opacity", 0);
 
     let bumpChart = new BumpChart(seasonData, updateGame, updateTeam);
     let finalSeasonChart = new SeasonTable(seasonData, updateTeam);
     let gameTable = new GameTable(seasonData, updateGame, updateTeam);
     let gameDetail = new GameDetail(seasonData, updateGame);
 
-    // Clears a finalSeasonChart selection by listening for a click
-    var seasonChartElement = document.getElementById('season-chart');
-    seasonChartElement.addEventListener("click", function (e) {
-        updateTeam(null);
-    }, true);
 });
 
 async function loadData(filename) {
     let data = await d3.csv(filename);
     return data;
 }
-
-/**
- * Returns html that can be used to render the tooltip.
- * @param data
- * @returns {string}
- */
-function tooltipRender(data) {
-		let text = "<span class='tooltiptext'>Team: " + data.team + "<br>Points: " + data.points + "</span>";
-		return text;
-	}
-
